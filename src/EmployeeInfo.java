fade5d3b1b1e5cc75587f864d8c888ac1bda014b
kushalation <kush.kumar@alationdata.com>
2019-07-13 17:46:44 +0530

    Second commit on EmployeeInfoUpdate branch

--- src/EmployeeInfo.java
+++ src/EmployeeInfo.java
@@ -12,7 +12,10 @@ public class EmployeeInfo {
 
         employee.setDepartment(department);
 
-        System.out.println(employee.toString());
-    }
+        System.out.println(employee.getId());
+        System.out.println(employee.getName());
+        System.out.println(employee.getDesignation());
+        System.out.println(employee.getDepartment().getId());
+        System.out.println(employee.getDepartment().getName());    }
 
 }
