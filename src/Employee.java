1da37c6a9b0d9d4c64ee95055b13e1818d56e384
kushalation <kush.kumar@alationdata.com>
2019-07-13 17:29:05 +0530

    First commit

---
+++ src/Employee.java
@@ -0,0 +1,52 @@
+public class Employee {
+
+    private String id;
+
+    private String name;
+
+    private String designation;
+
+    private Department department;
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", designation='" + designation + '\'' +
+                ", department=" + department +
+                '}';
+    }
+}
---
+++ src/EmployeeInfo.java
@@ -0,0 +1,18 @@
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setId("01");
+        employee.setDesignation("Developer");
+        employee.setName("Tom");
+
+        Department department = new Department();
+        department.setId("01");
+        department.setName("Engg");
+
+        employee.setDepartment(department);
+
+        System.out.println(employee.toString());
+    }
+
+}
